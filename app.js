beeb61c95f0d7cd78e3eedf1a241517c298ff419
小华 <158972928@qq.com>
2018-06-27 09:57:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 var allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  res.header('Access-Control-Allow-Headers', 'token')
+  res.header('Access-Control-Allow-Headers', 'token, Content-Type')
   res.header('Access-Control-Allow-Credentials', 'true')
   next()
 }
